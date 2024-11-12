24e19a206b2c4c0c993e68f6dad5098d9b663fd1
Lukas <lukaskarstens07@gmail.com>
2024-11-12 13:39:52 +0100

    supabase things

--- index.js
+++ index.js
@@ -22,14 +22,52 @@ app.get("/products", (req, res) => {
   res.send("Dette er produkter siden...");
 });
 
-app.get("/test", async (req, res) => {
+app.get("/songs", async (req, res) => {
   let { data, error } = await supabase.from("songs").select("id, title");
   if (error) {
     throw new Error(error.message);
-  } else {
-    console.log(data);
-    return data;
   }
+
+  let html = "<h1>Songs</h1><ul>";
+  data.forEach((song) => {
+    html += `<li>${song.id}: ${song.title}</li>`;
+  });
+  html += "</ul>";
+
+  res.send(html);
+  res.end();
+});
+
+app.get("/artists", async (req, res) => {
+  let { data, error } = await supabase.from("artists").select("*");
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  let html = "<h1>Artists</h1><ul>";
+  data.forEach((artist) => {
+    html += `<li><span style="font-weight: bold">${artist.name}</span> ${artist.description}</li>`;
+  });
+  html += "</ul>";
+
+  res.send(html);
+  res.end();
+});
+
+app.get("/albums", async (req, res) => {
+  let { data, error } = await supabase.from("albums").select("title, image");
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  let html = "<h1>Albums</h1><ul>";
+  data.forEach((album) => {
+    html += `<li><img src="${album.image}" alt="${album.title}" /> ${album.title}</li>`;
+  });
+  html += "</ul>";
+
+  res.send(html);
+  res.end();
 });
 
 app.listen(process.env.PORT, () => {

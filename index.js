d70e67c2eb3e3ae14289c218bc98daaf14d7dd7b
Lukas <lukaskarstens07@gmail.com>
2024-11-12 10:21:26 +0100

    exporess install

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hej verden!");
+});
+
+app.get("/about", (req, res) => {
+  res.send("Dette er about siden...");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("Dette er kontakt siden...");
+});
+
+app.listen(4242, () => {
+  console.log("Express server kører....");
+});

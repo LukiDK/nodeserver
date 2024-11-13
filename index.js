c2186b7fcca26e13f063b501f506cbe5d2ec6d3f
Lukas <lukaskarstens07@gmail.com>
2024-11-13 10:35:03 +0100

    Person calsses

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { supabase } from "./config/configSupabase.js";
+import { Person, Age, Card } from "./assets/js/persons.js";
 
 dotenv.config();
 
@@ -8,18 +9,6 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hej verden!");
-});
-
-app.get("/about", (req, res) => {
-  res.send("Dette er about siden...");
-});
-
-app.get("/contact", (req, res) => {
-  res.send("Dette er kontakt siden...");
-});
-
-app.get("/products", (req, res) => {
-  res.send("Dette er produkter siden...");
 });
 
 app.get("/songs", async (req, res) => {
@@ -44,8 +33,6 @@ app.get("/artists", async (req, res) => {
     throw new Error(error.message);
   }
 
-
-
   let html = "<h1>Artists</h1><ul>";
   data.forEach((artist) => {
     html += `<li><span style="font-weight: bold">${artist.name}</span><img style="max-width: 100px" src="${process.env.SUPABASE_URL}/storage/v1/object/public/images/${artist.image}"/></li>`;
@@ -69,6 +56,48 @@ app.get("/albums", async (req, res) => {
   html += "</ul>";
 
   res.send(html);
+  res.end();
+});
+
+app.get("/persons", async (req, res) => {
+  let person1 = {
+    fname: "John",
+    lname: "Doe",
+    mail: "JohnDoegmail.com",
+    birthday: 1990,
+    job: "Baker",
+    gender: "male",
+  };
+  let person2 = {
+    fname: "Jane",
+    lname: "Doe",
+    mail: "JaneDoegmail.com",
+    birthday: 1995,
+    job: "Carpenter",
+    gender: "male",
+  };
+  let person3 = {
+    fname: "Mike",
+    lname: "Oxlong",
+    mail: "MikeOxlong@gmail.com",
+    birthday: 1942,
+    job: "Web Developer",
+    gender: "male",
+  };
+
+  let persones = [person1, person2, person3];
+
+  persones.forEach((person) => {
+    let greetings = new Person(person.fname, person.lname);
+    console.log(greetings.present());
+
+    let card = new Card(person.fname, person.lname, person.mail, person.job);
+    console.log(card.present());
+
+    let age = new Age(person.birthday);
+    console.log(age.present());
+  });
+
   res.end();
 });
 
